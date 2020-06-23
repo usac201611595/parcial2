63cb535a1c44bd1e6515876997fe3672629a695a
usac201612268 <3003830370101@ingenieria.usac.edu.gt>
2020-06-23 17:25:20 -0600

    cliente clases metiendo funciones

--- cliente_MQTT_clases.py
+++ cliente_MQTT_clases.py
@@ -12,34 +12,21 @@ TOPICS_CHAT="usuario/12/"
 usuarios='usuarios.txt' # MGHP variable que representa el archivo donde se tienen guardado los usuarios
 salas='salas.txt'
 
-
-class clienteMQTT(object):
-
-    def __init__(self, ): # valores de entrada que no cambiaran y seran globales
-        pass
-
-    def 
-
 # LARP Configuracion inicial de logging
-logging.basicConfig(
+logging.basicConfig(    
     level = logging.INFO,
     format = '[%(levelname)s] (%(processName)-10s) %(message)s'
     )
 
-#class Negocia(object):
-#    def __init__(self,topic,contenidom):#GAMS
-#        self.topic=str(topic)#GAMS
-#        self.contenidom = contenidom#GAMSs
-                                    #)
-#    def alive(self):
-#        t1 = threading.Thread( target = self.s_alive,
-#                                        daemon = True
-#                                        )
-#        t1.start()
-#    def ACK(self):#GAMS
-#        return self.recepcion()[0]==b'x05' and self.recepcion()[1].decode("utf-8")=="201611595" and self.topic=="comandos/12"#GAMS
-def recepcion(topic,contenidom):
-    logging.info("si estoy en la funcion")
+class clienteMQTT(object): # LARP clase de cliente mqtt
+
+    def __init__(self, ): # LARP valores de entrada que no cambiaran y seran globales
+        pass
+
+    def 
+
+    def recepcion(topic,contenidom):
+        logging.info("si estoy en la funcion")
     #MGHP aqui empezamos a partir la informacion
     la_info=contenidom
     eltopic=topic.split('/')
@@ -61,13 +48,25 @@ def recepcion(topic,contenidom):
     else:
         datos2=la_info.split(b'$')
         print(datos2[0].decode("utf-8")+":"+datos2[1].decode("utf-8"))
-    #if topic=="comandos/12" and datos2[0]==b'\x04': #MGHP condicion para verificar el ALIVE
-        #logging.info("estoy recibiendo un alive de: " + str(datos2[1]))
 
-def hiloReproducion(entrada, num): #LARP 'funcion del hilo Reproduccion de fondo'
-    logging.info('Grabacion finalizada, inicia reproduccion') # LARP mensaje al finalizar
-    os.system('aplay ' + entrada) #LARP ejecutar en consola con aplay
-    time.sleep (num)
+    def hiloReproducion(entrada, num): #LARP 'funcion del hilo Reproduccion de fondo'
+        logging.info('Grabacion finalizada, inicia reproduccion') # LARP mensaje al finalizar
+        os.system('aplay ' + entrada) #LARP ejecutar en consola con aplay
+        time.sleep (num)
+        
+#class Negocia(object):
+#    def __init__(self,topic,contenidom):#GAMS
+#        self.topic=str(topic)#GAMS
+#        self.contenidom = contenidom#GAMSs
+                                    #)
+#    def alive(self):
+#        t1 = threading.Thread( target = self.s_alive,
+#                                        daemon = True
+#                                        )
+#        t1.start()
+#    def ACK(self):#GAMS
+#        return self.recepcion()[0]==b'x05' and self.recepcion()[1].decode("utf-8")=="201611595" and self.topic=="comandos/12"#GAMS
+
 
 # Tiempo de espera entre lectura y envio de dato de sensores a broker (en segundos)
 
